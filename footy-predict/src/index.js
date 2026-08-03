3ace4860ae531d3ac721780f9027c8b9ecf7b9ca
ziontech-admin <tonnymuuo90@gmail.com>
2026-08-03 11:27:56 +0300

    Add more leagues to the league list

--- footy-predict/src/index.js
+++ footy-predict/src/index.js
@@ -29,6 +29,11 @@ const LEAGUES = [
   { code: "SA", name: "Serie A" },
   { code: "BL1", name: "Bundesliga" },
   { code: "FL1", name: "Ligue 1" },
+  { code: "CL", name: "Champions League" },
+  { code: "DED", name: "Eredivisie" },
+  { code: "PPL", name: "Primeira Liga" },
+  { code: "ELC", name: "Championship" },
+  { code: "BSA", name: "Brazilian Série A" },
 ];
 
 const app = express();
